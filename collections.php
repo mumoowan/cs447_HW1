f3c942dba73b23d6157cb0f67de6eacd845d1f3b
mumoowan <0623sweet@gmail.com>
2017-09-06 12:13:10 +0700

    add collection

---
+++ collections.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>JustSaySweet</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+	<link rel="stylesheet" type="text/css" href="style/bootstrap.min.css">
+	<link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap-glyphicons.css" rel="stylesheet">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <style>
+	  .margin-bottom {
+	  	margin-bottom: 8px;
+	  }
+ 
+  </style>
+</head>
+<body>
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="index.php">JustSaySweet</a>
+    </div>
+
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav navbar-right">
+        <li class="active"><a href="#">Collections</a></li>
+        <li><a href="signin.php">Sign in</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+
+<div class="container">
+   <div class="row">
+    <div class="col-sm-4"> 
+    	<img src="pic/earring2.jpg" width="350" height="350" class="margin-bottom">
+    	<p> Name : Where Is Home Earrings </p>
+    	<p> Price : 300 Bath. </p>
+    	<p>Product Code : P000BCLI</p>
+    	<p>Amount : 12</p>
+    </div>
+    <div class="col-sm-4">
+  
+    	<img src="pic/earring3.jpg" width="350" height="350" class="margin-bottom">
+    
+    	<p> Name : Unmatched Love Earrings </p>
+    	<p> Price : 300 Bath.</p>
+    	<p>Product Code : P000BFQJ</p>
+    	<p>Amount : 3</p>
+    </div>
+    <div class="col-sm-4"> 
+    
+    	<img src="pic/earring1.jpg" width="350" height="350" class="margin-bottom">
+  
+    	<p> Name : Spring Fantasy Earrings </p>
+    	<p> Price : 365 Bath. </p>
+    	<p>Product Code : P000BCLG </p>
+    	<p>Amount : 22</p>
+    </div>
+    <div class="col-sm-4"> 
+    	
+    	<img src="pic/earring4.jpg" width="350" height="350" class="margin-bottom">
+ 
+    	<p> Name : My Mind's Flower Earrings </p>
+    	<p> Price : 460 Bath. </p>
+    	<p>Product Code : P000BCZW</p>
+    	<p>Amount : 8</p>
+    </div>
+    <div class="col-sm-4">
+    	
+    	<img src="pic/earring5.jpg" width="350" height="350"class="margin-bottom">
+
+    	<p> Name : Miss Ya Boy Earrings </p>
+    	<p> Price : 365 Bath.</p>
+    	<p>Product Code : P000BCJF </p>
+    	<p>Amount : 10</p>
+    </div>
+    <div class="col-sm-4"> 
+    	
+    	<img src="pic/earring6.jpg" width="350" height="350" class="margin-bottom">
+
+    	<p> Name : My Little Dandelion Earrings </p>
+    	<p> Price : 395 Bath.</p>
+    	<p>Product code : P000BFDU</p>
+    	<p>Amount : 15</p>
+    </div>
+
+
+</div>
+</div>
+</div>
+
+
+</div>
+</body>
+</html>
