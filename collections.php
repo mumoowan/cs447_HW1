b44d1bf923780a557314a449e4f88e491b9f9357
mumoowan <0623sweet@gmail.com>
2017-09-06 14:14:26 +0700

    modify collections

--- collections.php
+++ collections.php
@@ -24,6 +24,7 @@
 
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav navbar-right">
+        <li><a href="add.php">Add</a></li>
         <li class="active"><a href="#">Collections</a></li>
         <li><a href="signin.php">Sign in</a></li>
       </ul>
